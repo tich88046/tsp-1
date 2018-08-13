96791a3c0213d7dcf1ce756a2aa3701972c4a4f0
hunterlum <hunterlum@gmail.com>
2018-08-12 23:07:24 -0700

    resolved symlink issue

---
+++ java/Sol.java
@@ -0,0 +1,12 @@
+package tsp;
+
+public class Sol {
+	public int [] path;
+	public double fitness;
+/*	
+	public Sol(int [] arr, double fit) {
+		path=arr;
+		fitness=fit;
+	}
+	*/
+}
---
+++ java/main.java
@@ -0,0 +1,31 @@
+package tsp;
+import java.io.*;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] array=new int [101];
+		double fit=1.1;
+		Sol a=new Sol();
+		a.path=array;
+		a.fitness=fit;
+		System.out.println(a.path );
+		System.out.println(a.fitness);
+		
+		
+		File data=new File("/Users/hunterlum/Desktop/tsp/data/cities.csv");
+		try {
+		BufferedReader br = new BufferedReader(new FileReader(data));
+		String st;
+		while ((st = br.readLine()) != null){
+			System.out.println(st);
+		}
+		br.close();
+		}
+		catch(IOException e) {
+			System.err.println("An IOException was caught :"+e.getMessage());
+		}
+
+	}
+
+}
